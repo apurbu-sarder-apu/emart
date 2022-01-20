8dac806c2820d57aaf1e45ddc668aafd00dfa179
Apurbu Sarder <apurbusarderjust321@gmail.com>
2022-01-21 02:52:49 +0600

    PrivateRoute, Redirect

--- src/App.js
+++ src/App.js
@@ -1,29 +1,33 @@
 import './App.css';
 import Home from './component/Home';
 import Navbar from './component/Navbar';
-import {Switch , Route} from "react-router-dom";
+import {Switch , Route, Redirect} from "react-router-dom";
 import Products from "./component/Products";
 import Product from './component/Product';
 import Cart from './component/Cart';
 import Signup from './component/Signup';
 import Login from './component/Login';
-
+import { AuthProvider } from './component/context/AuthContext';
+import PrivateRoute from './component/PrivateRoute';
 
 function App() {
   return (
-      <>
+      <div>
+      <AuthProvider>
       <Navbar/>
       <Switch >
         <Route exact path="/" component={Home}/>
         <Route exact path="/products" component={Products}/>
         <Route exact path="/products/:id" component={Product}/>
-        <Route exact path="/cart" component={Cart}/>
+        <PrivateRoute exact path="/cart" component={Cart}/>
         <Route exact path="/signup" component={Signup}/>
         <Route exact path="/login" component={Login}/>
+        <Redirect from="/" to="/cart"/>
 
 
       </Switch >
-      </>
+      </AuthProvider>
+      </div>
   );
 }
 
--- src/firebase.js
+++ src/firebase.js
@@ -1,16 +1,39 @@
-import firebase from "firebase/app"
-import "firebase/auth"
+// import firebase from "firebase/app"
+// import "firebase/auth"
 
-const app = firebase.initializeApp({
-    apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
-    authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
-    projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
-    appId: process.env.REACT_APP_FIREBASE_APP_ID,
-    measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
-})
+// const app = firebase.initializeApp({
+//     apiKey: process.env.REACT_APP_FIREBASE_API_KEY,
+//     authDomain: process.env.REACT_APP_FIREBASE_AUTH_DOMAIN,
+//     databaseURL: process.env.REACT_APP_FIREBASE_DATABASE_URL,
+//     projectId: process.env.REACT_APP_FIREBASE_PROJECT_ID,
+//     storageBucket: process.env.REACT_APP_FIREBASE_STORAGE_BUCKET,
+//     messagingSenderId: process.env.REACT_APP_FIREBASE_MESSAGING_SENDER_ID,
+//     appId: process.env.REACT_APP_FIREBASE_APP_ID,
+//     measurementId: process.env.REACT_APP_FIREBASE_MEASUREMENT_ID
+// })
 
-export const auth = app.auth()
-export default app;
+// export const auth = app.auth()
+// export default app;
+
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+import { getAuth } from "firebase/auth";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+// For Firebase JS SDK v7.20.0 and later, measurementId is optional
+const firebaseConfig = {
+  apiKey: "AIzaSyDwNOxKvbieoDvYHCtB21LIFKDbxhF9E8c",
+  authDomain: "emart-6acc8.firebaseapp.com",
+  databaseURL: "https://emart-6acc8-default-rtdb.firebaseio.com",
+  projectId: "emart-6acc8",
+  storageBucket: "emart-6acc8.appspot.com",
+  messagingSenderId: "10041907391",
+  appId: "1:10041907391:web:966abc3b499bbddb5e4e0a",
+  measurementId: "G-91L92SVXQF"
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
